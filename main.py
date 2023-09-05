b3c6962cd013fdd42aa9dd60734b1293c5088e8a
Alexey <brech_36@mail.ru>
2023-09-05 15:45:41 +0900

    add task 1 fix

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 # разность и количество элементов нужно ввести с клавиатуры.
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
-def func(f_num, s_num, t_num):
-    list_el = []
 
+list_el = []
+def func():
+
+    f_num = int(input('Введите первый элемент:'))
+    s_num = int(input("Введите шаг элементов:"))
+    t_num = int(input("Введите количество элементов:"))
     for i in range(t_num):
         if i == 0:
             list_el.append(f_num)
         else:
             f_num += s_num
             list_el.append(f_num)
-    print(list_el)
 
-func(int(input('Введите первый элемент:')),int(input("Введите шаг элементов:")),int(input("Введите количество элементов:")))
+def list_num():
+    for i in list_el:
+        print(i)
+func()
+list_num()
+
